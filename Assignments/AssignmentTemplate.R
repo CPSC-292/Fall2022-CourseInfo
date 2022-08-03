0c88238477e85128a78eab89e86fd1ea9247ef25
lindsaywaldrop <waldrop@chapman.edu>
2022-08-03 12:32:44 -0700

    Adding basic files

---
+++ Assignments/AssignmentTemplate.R
@@ -0,0 +1,15 @@
+#### Assignment number #####
+# Your name:
+# Section number:
+# Date: 
+
+#### Required Libraries ####
+# Put any required libraries necessary to run your code in this section. 
+library(rmarkdown) # This is an example, which you can remove. 
+
+#### Question a #### 
+# Part i
+x <- 1 
+
+# Part ii
+y <- 10
